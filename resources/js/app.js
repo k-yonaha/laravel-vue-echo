d6c5abba119e2cc1e7046c7cb1f1e3f6d441aa3f
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-11-06 12:24:22 +0900

    fix mdi/fontが重いらしいので変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,22 @@
-import './bootstrap';
+import "./bootstrap";
 
-import { createApp } from 'vue';
-import App from './App.vue';
-import { createVuetify } from 'vuetify';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createVuetify } from "vuetify";
 import router from "./router";
-import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css'
+import "vuetify/dist/vuetify.min.css";
+// import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
 const app = createApp(App);
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+});
 
 app.use(router).use(vuetify).mount("#app");
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -20,5 +20,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: false,
+    // forceTLS: true
+    // encrypted: true
 });
