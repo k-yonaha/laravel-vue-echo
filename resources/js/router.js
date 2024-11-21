2c8530b03d511f2b570f3afaff740434bd489923
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-11-21 17:55:28 +0900

    router.jsの設定変更

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,7 +4,7 @@ import Login from "./components/Login.vue";
 import store from './store';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.VITE_BASE_URL || '/'),
     routes: [
         {
             path: "/",
